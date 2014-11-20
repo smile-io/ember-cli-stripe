38ac3255298d4803439abc3f4e790b5e491d42db
Bill Curtis <wcurtis@sweettoothhq.com>
2014-11-20 15:35:45 -0500

    Add all stripe options to component and better comments

--- app/components/stripe-checkout.js
+++ app/components/stripe-checkout.js
@@ -2,33 +2,132 @@ import Ember from 'ember';
 import config from '../config/environment';
 
 /**
- * Stripe checkout component that displays the pay button
- * and opens stripe modal to accept payment.
+ * Stripe checkout component for accepting payments with
+ * an embedded form.
+ * 
+ * Stripe docs: https://stripe.com/docs/tutorials/checkout
  *
  * Usage:
- * {{stripe-checkout description=billingPlan.description amount=billingPlan.amount}}
- * 
- * Docs: https://stripe.com/docs/tutorials/checkout
+ * {{stripe-checkout 
+ *   description=billingPlan.description 
+ *   amount=billingPlan.amount
+ * }}
+ *
+ * TODO:
+ * Implement opened/closed callbacks as actions
+ * Support default stripe styles
  */
 export default Ember.Component.extend({
-
   tagName: 'button',
-  classNames: ['btn', 'btn-primary'],
+  classNames: ['stripe-checkout'],
   attributeBindings: ['isDisabled:disabled'],
 
-  name: "Sweet Tooth",
-  panelLabel: null,
-  amount: 1000,
-  description: "Small Business ($10.00)",
-  key: config['stripe-checkout'].key,
-  image: "/images/logo-jar.png",
-  isDisabled: false,
-
-  handler: null,
-  token: null,
+  /**********************************
+   * Required attributes
+   **********************************/
 
   /**
-   * Token looks like this
+   * Your publishable key (test or live).
+   */
+  key: config.stripe.key,
+
+  /**********************************
+   * Highly recommended attributes
+   **********************************/
+
+  /**
+   * A relative URL pointing to a square image of your brand or 
+   * product. The recommended minimum size is 128x128px.
+   * Eg. "/square-image.png"
+   */
+  image: null,
+
+  /**
+   * The name of your company or website.
+   */
+  name: "Demo Site",
+
+  /**
+   * A description of the product or service being purchased.
+   */
+  description: "2 widgets ($20.00)",
+
+  /**
+   * The amount (in cents) that's shown to the user. Note that you 
+   * will still have to explicitly include it when you create a 
+   * charge using the Stripe API.
+   */
+  amount: 2000,
+
+  /**********************************
+   * Optional attributes
+   **********************************/
+  
+  /**
+   * The currency of the amount (3-letter ISO code). The default is USD.
+   */
+  currency: "USD",
+
+  /**
+   * The label of the payment button in the Checkout form (e.g. “Subscribe”, 
+   * “Pay {{amount}}”, etc.). If you include {{amount}}, it will be replaced 
+   * by the provided amount. Otherwise, the amount will be appended to the 
+   * end of your label.
+   */
+  panelLabel: null,
+
+  /**
+   * Specify whether Checkout should validate the billing ZIP code 
+   * (true or false). The default is false.
+   */
+  zipCode: false,
+
+  /**
+   * If you already know the email address of your user, you can provide 
+   * it to Checkout to be pre-filled.
+   */
+  email: null,
+
+  /**
+   * The text to be shown on the default blue button.
+   */
+  label: "Pay with card", 
+
+  /**
+   * Specify whether to include the option to "Remember Me" for future 
+   * purchases (true or false). The default is true.
+   */
+  allowRememberMe: true,
+
+  /**********************************
+   * Extras
+   **********************************/
+
+  /**
+   * Bind to this attribute to disable the stripe
+   * button until the user completes prior requirements
+   * (like choosing a plan)
+   */
+  isDisabled: false,
+
+  /**
+   * Stripe handler
+   */
+  handler: null,
+
+  /**
+   * By default we add stripe button classes.
+   * Set to false to disable Stripe styles
+   *
+   * TODO: Need to load stripe styles in order for this to apply
+   */
+  useStripeStyles: true,
+
+  /**
+   * Sets up Stripe and sends component action
+   * with the Stripe token when checkout succeeds.
+   * 
+   * The token looks like this
    * {
    *   "id": "tok_150enDGA2quO03uZPF8Nve2a",
    *   "livemode": false,
@@ -70,18 +169,27 @@ export default Ember.Component.extend({
         self.sendAction('action', token);
       }
     });
-
     this.set('handler', handler);
   }.on('init'),
 
+  /**
+   * Kick up the modal if we're clicked
+   */
   click: function(e) {
+    this.openModal();
+    e.preventDefault();
+  },
+
+  /**
+   * Opens the Stripe modal for payment
+   */
+  openModal: function() {
     this.get('handler').open({
       name: this.get('name'),
       description: this.get('description'),
       amount: this.get('amount'),
       panelLabel: this.get('panelLabel')
     });
-    e.preventDefault();
   },
 
   willDestroy: function() {
