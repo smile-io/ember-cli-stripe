e974f922ba276451252cbff0a174bb469c7ab124
Matt McManus <matt@mcmanus.io>
2015-09-28 10:16:16 -0400

    Upgrade to ember-cli 1.13.8

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -4,7 +4,7 @@ import Router from '../../router';
 import config from '../../config/environment';
 
 export default function startApp(attrs) {
-  var App;
+  var application;
 
   var attributes = Ember.merge({}, config.APP);
   attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
@@ -14,12 +14,11 @@ export default function startApp(attrs) {
   });
 
   Ember.run(function() {
-    App = Application.create(attributes);
-    App.setupForTesting();
-    App.injectTestHelpers();
+    application = Application.create(attributes);
+    application.setupForTesting();
+    application.injectTestHelpers();
   });
 
-  App.reset(); // this shouldn't be needed, i want to be able to "start an app at a specific URL"
-
-  return App;
+  application.reset(); // this shouldn't be needed, i want to be able to "start an app at a specific URL"
+  return application;
 }
