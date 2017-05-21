2d2a1b714fe6d787d46f5bb94de51438f05c2296
Vlad Vanca <vanca.vlad@gmail.com>
2017-05-21 19:49:31 +0200

    Move ember-invoke-action into dependencies instead of dev-dependencies. fix #28

--- app/initializers/stripe.js
+++ app/initializers/stripe.js
@@ -1,11 +1,9 @@
 import config from '../config/environment';
-import Stripe from 'ember-cli-stripe/services/stripe';
 
 export function initialize() {
   const application = arguments[1] || arguments[0];
-  const { stripe } = config || {};
+  const { stripe = {} } = config;
 
-  application.register('service:stripe', Stripe);
   application.register('config:stripe', stripe, { instantiate: false });
   application.inject('service:stripe', 'stripeConfig', 'config:stripe');
 }
