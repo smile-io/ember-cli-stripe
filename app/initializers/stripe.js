e8be2e579989a2888f3b72bbfd934a1ac73a6cc6
Mariano Gonzalez <marianoggf@gmail.com>
2024-04-08 11:40:44 -0300

    Simplify initializer

--- app/initializers/stripe.js
+++ app/initializers/stripe.js
@@ -1,14 +1,6 @@
-import config from '../config/environment';
-
-export function initialize() {
-  const application = arguments[1] || arguments[0];
-  const { stripe = {} } = config;
-
-  application.register('config:stripe', stripe, { instantiate: false });
-  application.inject('service:stripe', 'stripeConfig', 'config:stripe');
-}
+export function initialize() {}
 
 export default {
   name: 'stripe',
-  initialize
+  initialize,
 };
